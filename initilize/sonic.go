5171007388c153f61fa33053c162358ec7013a5b
dry-indus <dry_indus@163.com>
2022-11-17 00:41:26 +0700

    调整sonic

--- initilize/sonic.go
+++ initilize/sonic.go
@@ -10,13 +10,7 @@ import (
 )
 
 func initSonic() {
-	var err error
 	cfg := global.Sonic
-	ingester, err := gosonic.NewIngester(cfg.Host, cfg.Port, cfg.Password)
-	if err != nil {
-		panic(err)
-	}
-
 	global.SONIC_INGESTER_CH = make(chan *global.SonicBulkPushEvent)
 
 	go func() {
@@ -27,43 +21,57 @@ func initSonic() {
 				parallelRoutines = cfg.ParallelRoutines
 			}
 			lang := getLang(e.Lang)
+
+			ingester, err := gosonic.NewIngester(cfg.Host, cfg.Port, cfg.Password)
+			if err != nil {
+				global.DL_LOGGER.Errorf("sonic new ingester failed! trace: %s, errs: %s", e.Trace, util.MustMarshalToString(err))
+				continue
+			}
+
 			errs := ingester.BulkPush(e.Collection, e.Bucket, parallelRoutines, e.Records, lang)
 			global.DL_LOGGER.Debugf("sonic bulk push finish! trace: %s, records:%s, lang: %s, errs: %s", e.Trace, util.MustMarshalToString(e.Records), lang, util.MustMarshalToString(errs))
 			if len(errs) != 0 {
 				global.DL_LOGGER.Errorf("sonic bulk push failed! trace: %s, records:%s, lang: %s, errs: %s", e.Trace, util.MustMarshalToString(e.Records), lang, util.MustMarshalToString(errs))
 			}
+
+			ingester.Quit()
 		}
 	}()
-
-	search, err := gosonic.NewSearch(cfg.Host, cfg.Port, cfg.Password)
-	if err != nil {
-		panic(err)
-	}
 
 	global.SONIC_SEARCH_CH = make(chan *global.SonicSearchEvent)
 
 	go func() {
 		for e := range global.SONIC_SEARCH_CH {
+			search, err := gosonic.NewSearch(cfg.Host, cfg.Port, cfg.Password)
+			if err != nil {
+				global.DL_LOGGER.Errorf("sonic new search failed! trace: %s, errs: %s", e.Trace, util.MustMarshalToString(err))
+				continue
+			}
 			result := &global.SonicSearcResult{}
 			result.Results, result.Err = search.Query(e.Collection, e.Bucket, e.Terms, e.Limit, e.Offset, getLang(e.Lang))
 			global.DL_LOGGER.Debugf("sonic search query finish! trace: %s, results: %s, errs: %v ", e.Trace, result.Results, result.Err)
 			e.Result <- result
+
+			search.Quit()
 		}
 	}()
-
-	suggest, err := gosonic.NewSearch(cfg.Host, cfg.Port, cfg.Password)
-	if err != nil {
-		panic(err)
-	}
 
 	global.SONIC_SUGGEST_CH = make(chan *global.SonicSuggestEvent)
 
 	go func() {
 		for e := range global.SONIC_SUGGEST_CH {
+			suggest, err := gosonic.NewSearch(cfg.Host, cfg.Port, cfg.Password)
+			if err != nil {
+				global.DL_LOGGER.Errorf("sonic new suggest failed! trace: %s, errs: %s", e.Trace, util.MustMarshalToString(err))
+				continue
+			}
+
 			result := &global.SonicSearcResult{}
 			result.Results, result.Err = suggest.Suggest(e.Collection, e.Bucket, e.Word, e.Limit)
 			global.DL_LOGGER.Debugf("sonic search suggest finish! trace: %s, results: %s, errs: %v ", e.Trace, result.Results, result.Err)
 			e.Result <- result
+
+			suggest.Quit()
 		}
 	}()
 }
